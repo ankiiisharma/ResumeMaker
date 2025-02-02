2d6ac5fafdc9701d8d7c4cb2571dc899130244f8
Ankiiisharma <myselfankit.2003@gmail.com>
2025-02-02 13:55:55 +0530

    fix: home page fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,56 +1,63 @@
-import Image from 'next/image';
-import ResumeTemplate from "../public/resume.jpg"
-import Navbar from './Navbar';
+"use client";
+
+import { FaLinkedin } from "react-icons/fa6";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import Navbar from "./Navbar";
+import { GrContactInfo } from "react-icons/gr";
 
 const Hero = () => {
   return (
-    <> 
+    <>
       <Navbar />
-      <section className='hero mt-[150px] px-4 md:px-8 lg:px-16'>
-        <div className='relative'>
-          <div className='absolute left-[-100px] top-0 transform rotate-[6deg] transition-transform duration-300 ease-in-out group-hover:rotate-0 overflow-hidden z-0'>
-            <Image 
-              src={ResumeTemplate} 
-              alt='resume template' 
-              className='object-cover rounded-md border border-gray-400 transform hover:translate-y-[-8px] ease-linear duration-150' 
-              width={800} 
-              height={600} 
-              style={{ objectPosition: 'top', objectFit: 'cover' }} 
-            />
-          </div>
-          <div className='relative flex flex-col justify-center p-4 md:p-10 ml-[720px] z-10'>
-            <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight'>
-              Create an ATS friendly
-            </h1>
-            <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight mt-2'>
-              Resume in 5 minutes!
-            </h1>
-            <h3 className='text-xl md:text-2xl lg:text-3xl tracking-tighter font-semibold mt-5'>
-              Helping you create a professional resume in minutes.
-            </h3>
-            <div className='mt-5'>
-              <button className='bg-blue-500 text-white px-5 py-2 rounded-md hover:bg-blue-600 transition-colors duration-200'>
-                Click here to get started
-              </button>
-            </div>
-          </div>
-        </div>
+      <section className="hero mt-[150px] px-4 md:px-8 lg:px-16 w-full mx-auto flex flex-col justify-center items-center text-center">
+        <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold tracking-[-2px] md:tracking-[-3px] lg:tracking-[-5px]">
+          Make{" "}
+          <span className="text-blue-400">Professional and ATS friendly</span>
+        </h1>
+        <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold tracking-[-2px] md:tracking-[-3px] lg:tracking-[-5px]">
+          Resume in just <span className="text-blue-400">5 minutes !!</span>
+        </h1>
+        <div className="w-full md:w-[500px] lg:w-[700px] h-1 bg-gradient-to-r from-transparent via-blue-300 to-transparent mt-3" />
 
-        <div className='relative w-full mt-10 p-4 md:p-10 bg-gray-100 rounded-md z-20'>
-          <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold tracking-tighter leading-tight'>
-            Why choose our resume builder?
-          </h2>
-          <ul className='list-disc list-inside mt-4 text-lg md:text-xl lg:text-2xl'>
-            <li>Easy to use and customize</li>
-            <li>Professional templates</li>
-            <li>ATS friendly</li>
-            <li>Save and download in multiple formats</li>
-          </ul>
-        </div>
+        <div className="grid grid-cols-3 md:grid-cols-3 gap-4 mt-4">
+          <button className="bg-blue-500 py-2 px-3 hover:bg-blue-600 hover:translate-y-[-2px] duration-150 ease-in-out font-semibold tracking-[-1px] rounded-md border border-slate-50/10 flex items-center">
+            <GrContactInfo className="mr-2 h-5" /> Fill data manually
+          </button>
 
+          <button
+            className="relative bg-blue-500 py-2 px-3 hover:bg-gray-500 cursor-not-allowed font-semibold tracking-[-1px] rounded-md border border-slate-50/10 flex items-center"
+            onMouseEnter={(e) => {
+              const tooltip = document.createElement("div");
+              tooltip.innerHTML = "Feature coming soon, thanks 😊";
+              tooltip.className =
+                "absolute left-1/2 bottom-full mb-2 bg-gray-700/90 border border-gray-100/20 text-white font-normal text-sm p-2 rounded-md shadow-lg transform -translate-x-1/2 whitespace-nowrap";
+              e.currentTarget.appendChild(tooltip);
+              e.currentTarget.onmouseleave = () => {
+                tooltip.remove();
+              };
+            }}
+          >
+            <FaLinkedin className="mr-2 h-5" /> Login with LinkedIn
+          </button>
+          <button
+            className="relative bg-blue-500 py-2 px-3 hover:bg-gray-500 cursor-not-allowed font-semibold tracking-[-1px] rounded-md border border-slate-50/10 flex items-center"
+            onMouseEnter={(e) => {
+              const tooltip = document.createElement("div");
+              tooltip.innerHTML = "Feature coming soon, thanks 😊";
+              tooltip.className =
+                "absolute left-1/2 bottom-full mb-2 bg-gray-700/90 border border-gray-100/20 text-white font-normal text-sm p-2 rounded-md shadow-lg transform -translate-x-1/2 whitespace-nowrap";
+              e.currentTarget.appendChild(tooltip);
+              e.currentTarget.onmouseleave = () => {
+                tooltip.remove();
+              };
+            }}
+          >
+            <FaCloudUploadAlt className="mr-2 h-5" /> Upload old Resume
+          </button>
+        </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
